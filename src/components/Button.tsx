b115c4d211269ba87958ab1b4a18461049fbc1fb
schr0cat <maksimhlipitko@gmail.com>
2026-09-09 15:37:24 +0300

    Добавить компонент кнопки

---
+++ src/components/Button.tsx
@@ -0,0 +1,50 @@
+import classNames from 'classnames';
+import { twMerge } from 'tailwind-merge';
+
+
+interface ButtonProps {
+  children: string | React.ReactSVGElement,
+  primary?: boolean,
+  secondary?: boolean,
+  success?: boolean,
+  warning?: boolean,
+  danger?: boolean,
+  outline?: boolean,
+  rounded?: boolean,
+  rest?: string,
+};
+
+function Button({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+  ...rest
+}: ButtonProps) {
+  const classes: string = twMerge(
+    classNames(rest.className, "flex items-center px-3 py-1.5 border", {
+      "border-blue-500 bg-blue-500 text-white": primary,
+      "border-gray-900 bg-gray-900 text-white": secondary,
+      "border-green-500 bg-green-500 text-white": success,
+      "border-yellow-400 bg-yellow-400 text-white": warning,
+      "border-red-500 bg-red-500 text-white": danger,
+      "rounded-full": rounded,
+      "bg-white": outline,
+      "text-blue-500": outline && primary,
+      "text-gray-900": outline && secondary,
+      "text-green-500": outline && success,
+      "text-yellow-400": outline && warning,
+      "text-red-500": outline && danger,
+    })
+  );
+
+  return <button {...rest} className={classes}>
+    {children}
+  </button>
+};
+
+export default Button;
