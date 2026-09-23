2f84a1a8ced441095ae4b14938203597f9806c9c
schr0cat <maksimhlipitko@gmail.com>
2026-09-23 19:37:58 +0300

    Написать код отправки файлов на сервер на клиенте. Начать разработку кода обработки полученных файлов на сервере

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,36 @@
 const express = require('express');
+import fileupload from "express-fileupload";
 const app = express()
+const path = require('path');
 const port = 3010
+const cors = require('cors');
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.json());
+
+app.use(cors({
+  origin: `http://localhost:5173`,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ["Content-Type, Authorization"]
+}));
+
+app.use(
+  fileupload({
+    createParentPath: true,
+  }),
+);
+
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/upload-files', (req, res) => {
+  console.log(req.body);
+  res.send('Все файлы были успешно получены');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
--- frontend/src/components/FileDownloadForm.tsx
+++ frontend/src/components/FileDownloadForm.tsx
@@ -6,12 +6,30 @@ interface FileDownloadFormInput {
   files: FileList;
 }
 
+
 function FileDownloadForm() {
   const { register, handleSubmit } = useForm<FileDownloadFormInput>();
-  const onSubmit: SubmitHandler<FileDownloadFormInput> = (data) => console.log(data);
+
+  const onSubmit: SubmitHandler<FileDownloadFormInput> = async (data) => {
+    const formData = new FormData();
+
+    for (let i = 0; i < data.files.length; i++) {
+      formData.append(`file${i}`, data.files[i]);
+    };
+
+    const res = await fetch('http://localhost:3010/upload-files', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: formData,
+      mode: "cors"
+    });
+    const resText = await res.text();
+    console.log(resText)
+  };
 
   return <form onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data">
-    {/* TODO: написать то, что файл должен быть именно pdf/docx и может быть добавлено сразу несколько файлов */}
     <input 
       {...register('files')} 
       type='file'
