c9524b078375ee58ad142fb27221a376d08097f8
schr0cat <maksimhlipitko@gmail.com>
2026-09-23 15:18:41 +0300

    Добавить container в App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,25 +11,27 @@ function App() {
   return <>
     <Header></Header>
 
-    <Route path='/'>
-      <div className='flex gap-x-8 mb-[45px]'>
-        <Link to='/addWords' className='bg-(--color-primary) text-lg text-(--color-white) p-[8px_28px] rounded-lg'>Добавить слово/слова</Link>
-        <Link to='/dictionary' className='bg-(--color-primary) text-lg text-(--color-white) p-[8px_70px] rounded-lg'>Словарь</Link>
-      </div>
+    <div className='container'>
+      <Route path='/'>
+        <div className='flex gap-x-8 mb-[45px]'>
+          <Link to='/addWords' className='bg-(--color-primary) text-lg text-(--color-white) p-[8px_28px] rounded-lg'>Добавить слово/слова</Link>
+          <Link to='/dictionary' className='bg-(--color-primary) text-lg text-(--color-white) p-[8px_70px] rounded-lg'>Словарь</Link>
+        </div>
 
-      <div className='flex gap-x-8'>
-        <RecentlyAdded />
-        <FileDownloadForm />
-      </div>
-    </Route>
+        <div className='flex gap-x-8'>
+          <RecentlyAdded />
+          <FileDownloadForm />
+        </div>
+      </Route>
 
-    <Route path='/addWords'>
-      <AddWordsPage />
-    </Route>
+      <Route path='/addWords'>
+        <AddWordsPage />
+      </Route>
 
-    <Route path='/dictionary'>
-      <DictionaryPage />
-    </Route>
+      <Route path='/dictionary'>
+        <DictionaryPage />
+      </Route>
+    </div>
   </>
 }
 
